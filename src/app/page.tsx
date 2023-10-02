5b99b38ff79aba4bb72d563f71eb751945decde9
Bryan Rivas <bryanjohann1995@gmail.com>
2023-10-02 19:41:30 -0400

    testar componentes client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
 'use client'
 import Link from 'next/link'
 import { Button } from '@nextui-org/button';
-// import { CustomButton, MainTitle } from './styles';
+import { CustomButton, MainTitle } from './styles';
 export default async function Home() {
   return (
     <main className="flex flex-col gap-3 justify-center items-center min-h-screen">
-      {/* <MainTitle>Home Page</MainTitle> */}
+      <MainTitle>Home Page</MainTitle>
       <Link href={'/admin'} className="font-bold">
         Panel Admin
       </Link>
       <div>
         {/* de lado cliente */}
       </div>
-      {/* <CustomButton isSubmit={true}>Other CLick me</CustomButton> */}
+      <CustomButton isSubmit={true}>Other CLick me</CustomButton>
       <Button onClick={ () => console.log('hola mundo')}>Click Me</Button>
     </main>
   )
